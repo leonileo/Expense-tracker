9ce83ed3e8b5e0c1d539c431dcfdd5eb2a1a09bb
kaleb <leolittleprogrammer@gmail.com>
2024-04-06 11:32:10 +0300

    Hot fix!

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const { db } = require('/db/DB.JS');
+const path = require('path')
+const p = path.dirname(__filename)
+const { db } = require(`${p}/db/DB.JS`);
 const {readdirSync} = require('fs');
 const app = express();
 require('dotenv').config();
 
 const PORT = process.env.PORT
 
+console.log(p);
 // middlewares
 app.use(express.json())
 app.use(cors())
